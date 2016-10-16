c76964c15dcc017312343691ac305f1fcc086d19
Kasper van den Berg <kasper@kaspervandenberg.net>
2016-10-16 14:06:21 +0200

    - Rename with-existing-entity2.

--- entity-component-system.lisp
+++ entity-component-system.lisp
@@ -79,7 +79,7 @@
     (f)))
 
 
-(defmacro with-existing-entity2 (entity &body body)
+(defmacro with-existing-entity* (entity &body body)
   `(with-existing-entity ,entity (lambda (entity) ,@body)))
 
 
@@ -95,7 +95,7 @@
 
 
 (defmethod (setf component-entity) (entity (component Component))
-  (with-existing-entity2 entity
+  (with-existing-entity* entity
     (setf (slot-value component 'entity) entity)))
 
 
@@ -109,7 +109,7 @@
   "Create a fresh-instance of `component-class` and add it to the components of 
    `entity`.
    `component-initargs` are supplied to `(make-instance component-class …)`."
-  (with-existing-entity2 entity
+  (with-existing-entity* entity
     (apply #'make-instance (append (list component-class :entity entity) component-initargs))))
 
 
@@ -125,7 +125,7 @@
   "Return all `Components` of `entity`.
    If `component-type` is specified, only the `Components` of `component-type`
    are returned otherwise all `Components` are returned."
-  (with-existing-entity2 entity
+  (with-existing-entity* entity
     (remove-if-not (lambda (x) (eql x entity)) 
                      (get-components-of-type component-type) 
                      :key #'entity)))
